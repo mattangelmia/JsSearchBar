c387460e1fcf31ae9a61433b6603b2e9e7028e0c
mattangelmia <23matthewangel@gmail.com>
2021-08-17 16:51:26 -0400

    search bar finished

---
+++ index.js
@@ -0,0 +1,21 @@
+let search = document.getElementById("searchInput");
+let allNamesDOMCollection = document.getElementsByClassName("name");
+console.log(allNamesDOMCollection[0].textContent);
+
+search.addEventListener("keyup", function (event) {
+  let searchQuery = event.target.value.toLowerCase();
+  console.log(searchQuery);
+  let allNamesDOMCollection = document.getElementsByClassName("name");
+  console.log(allNamesDOMCollection[0].textContent);
+
+  for (let i = 0; i < allNamesDOMCollection.length; i++) {
+    const currentName = allNamesDOMCollection[i].textContent.toLowerCase();
+    console.log(currentName);
+
+    if (currentName.includes(searchQuery)) {
+      allNamesDOMCollection[i].style.display = "block";
+    } else {
+      allNamesDOMCollection[i].style.display = "none";
+    }
+  }
+});
